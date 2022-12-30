ea263235fc1221405bda9f7a9fe46d6e6efb67dd
Saurabh <saurabhmehta1601@gmail.com>
2022-12-30 14:47:56 +0530

    style: apply css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react"
+import { CssBaseline } from "@mui/material"
 
 function App() {
 
   return (
-    <div >
-      <h1>Hello from fromt react app</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <div >
+        <h1>Hello from fromt react app</h1>
+      </div>
+    </>
   )
 }
 
