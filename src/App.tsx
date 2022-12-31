906b5c0aa01d6e4ea578f9745fbf072f36ea226d
Saurabh <saurabhmehta1601@gmail.com>
2022-12-31 13:51:31 +0530

    feat: setup app pages and their paths with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import { CssBaseline } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/Home"
+import LoginPage from "./pages/Login"
+import ProfilePage from "./pages/Profile"
 
 function App() {
 
   return (
     <>
       <CssBaseline />
-      <div >
-        <h1>Hello from fromt react app</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
