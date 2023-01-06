7c8606a1229f3c2245a4611c95e12d1f24c8d5dd
saurabh <saurabhmehta1601@gmail.com>
2023-01-06 21:16:13 +0530

    feat: complete post card

--- src/features/Post/index.tsx
+++ src/features/Post/index.tsx
@@ -5,20 +5,39 @@ import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
+import IconButton, { IconButtonProps } from '@mui/material/IconButton'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
 import CardActions from "@mui/material/CardActions"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import Collapse from '@mui/material/Collapse'
-import { red } from '@mui/material/colors'
+import { grey, red } from '@mui/material/colors'
+import { Box } from '@mui/material'
+import ShareIcon from '@mui/icons-material/Share';
+import { styled } from '@mui/material/styles'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }: any) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 const Post = () => {
     const [isPostLiked, setIsPostLiked] = React.useState(true)
     const [isPostExpanded, setIsPostExpanded] = React.useState(false)
     return (
-        <Card sx={{ padding: 2 }}>
+        <Card>
             <CardHeader
                 avatar={
                     <Avatar
@@ -35,28 +54,40 @@ const Post = () => {
                     </IconButton>
                 }
             />
-            <CardMedia
-                component="img"
-                image="https://images.unsplash.com/photo-1523287562758-66c7fc58967f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="Paella dish"
-                sx={{
-                    width: '100%', height: "275px", objectFit: "contain", objectPosition: "center",
-                    borderRadius: "12px"
-                }}
+            <CardMedia component="img"
+                alt="post image"
+                src="https://images.unsplash.com/photo-1523287562758-66c7fc58967f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             />
-            <CardContent >
-                <Typography variant="body1" color="text.primary">
+            <CardContent>
+                <Typography variant="h6" color="text.primary" >
                     Git and Github For Beginners
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton onClick={() => setIsPostLiked(prev => !prev)}>
-                    <FavoriteIcon htmlColor={isPostLiked ? red[600] : undefined} />
+                <IconButton onClick={() => setIsPostLiked(prev => !prev)} >
+                    <FavoriteIcon sx={{ opacity: 1, color: isPostLiked ? 'red' : grey[500] }} />
                 </IconButton>
+                <IconButton >
+                    <ShareIcon />
+                </IconButton>
+                <ExpandMore
+                    sx={{ marginLeft: "auto" }}
+                    onClick={() => setIsPostExpanded(prev => !prev)}
+                    expand={isPostExpanded}
+                    aria-expanded={isPostExpanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
 
             </CardActions>
 
-            <Collapse in={isPostExpanded}></Collapse>
+            <Collapse in={isPostExpanded} timeout="auto" unmountOnExit>
+                <Typography paragraph p={1}>
+                    Git is something everyone knows about
+                </Typography>
+            </Collapse>
+
         </Card>
     )
 }
