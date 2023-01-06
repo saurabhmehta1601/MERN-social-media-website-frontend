82d6287cc9076fef4a55b938957920dbf28cfdbb
saurabh <saurabhmehta1601@gmail.com>
2023-01-06 21:51:33 +0530

    feat: add navigation buttons to navbar

--- src/features/navbar/index.tsx
+++ src/features/navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Stack, Typography, useTheme } from '@mui/material'
+import { Button, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
 import { grey, indigo } from '@mui/material/colors';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -10,9 +10,14 @@ import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import LogoutIcon from '@mui/icons-material/Logout';
 import IconButton from '@mui/material/IconButton';
 import { logout } from '../../state/features/authSlice';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import ExploreIcon from '@mui/icons-material/Explore';
 
 const Navbar = () => {
     const { user } = useAppSelector(state => state.auth)
+    const theme = useTheme()
+    const isNonMobileScreen = useMediaQuery(theme.breakpoints.up('sm'))
+
     const dispatch = useAppDispatch()
     const handleLogout = () => {
         dispatch(logout())
@@ -21,20 +26,38 @@ const Navbar = () => {
         <Box p={2} sx={{ backgroundColor: indigo[500], color: grey[200], }} >
             <Container maxWidth={"md"}>
                 <Stack direction={"row"} justifyContent="space-between">
-                    <Typography variant="h4" fontWeight={"500"} fontFamily={"Rubik"}  >Gitgram</Typography>
-                    <Stack direction="row" spacing={2} alignItems="center">
-                        <Link component={RouterLink} to="/" underline="none" color="inherit" >
-                            <Button variant="contained">Feed</Button>
+                    <Typography variant="h4" fontWeight={"500"} fontFamily={"Rubik"}  >
+                        <Link component={RouterLink} to="/" underline='none' color={"white"}>
+                            Gitgram
+                        </Link>
+                    </Typography>
+
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <Link component={RouterLink} to="/explore" underline="none" color="inherit" >
+                            <IconButton >
+                                <ExploreIcon htmlColor='white'
+                                    fontSize={isNonMobileScreen ? 'large' : 'medium'}
+                                />
+                            </IconButton>
                         </Link>
                         <Link component={RouterLink} to="/create-post" underline="none" color="inherit" >
-                            <Button variant="contained">Create Post</Button>
+                            <IconButton >
+                                <PostAddIcon htmlColor='white'
+                                    fontSize={isNonMobileScreen ? 'large' : 'medium'}
+                                />
+                            </IconButton>
                         </Link>
 
-                        <IconButton onClick={handleLogout}>
-                            <LogoutIcon htmlColor='white' />
-                        </IconButton>
+                        <Avatar
+                            src={user ? "http://localhost:8000/uploads/" + user.profilePicture : ""}
+                            alt={user ? user.firstName + " " + user.lastName : ""}
+                        />
 
-                        <Avatar src={user ? "http://localhost:8000/uploads/" + user.profilePicture : ""} />
+                        <IconButton onClick={handleLogout}>
+                            <LogoutIcon htmlColor='white'
+                                fontSize={isNonMobileScreen ? 'large' : 'medium'}
+                            />
+                        </IconButton>
 
                     </Stack>
 
